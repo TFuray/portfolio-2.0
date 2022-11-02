dfb2640c15af8098f6843807f4c8432f0794f055
TFuray <pk.master881@gmail.com>
2022-11-01 23:59:17 -0700

    updating about page

--- assets/js/extras.js
+++ assets/js/extras.js
@@ -1,9 +1,24 @@
-function displayDate(){
+function displayDate() {
     let currentTime = new Date()
-    let month = currentTime.getMonth()+1
+    let month = currentTime.getMonth() + 1
     let day = currentTime.getDate()
     let year = currentTime.getFullYear()
-    document.getElementById('date').innerText = `${month} / ${day} / ${year}`
+    let hour = currentTime.getHours()
+    let minute = currentTime.getMinutes()
+
+    if (minute < 10){
+        minute = `0${minute}`
+    }
+
+    if (hour <= 12){
+    document.getElementById(
+        "date"
+    ).innerText = `${hour}:${minute}a.m. ${month} / ${day} / ${year}`
+    }else{
+            document.getElementById(
+                "date"
+            ).innerText = `${hour-12}:${minute}p.m. ${month} / ${day} / ${year}`
+    }
 }
 
 displayDate()
